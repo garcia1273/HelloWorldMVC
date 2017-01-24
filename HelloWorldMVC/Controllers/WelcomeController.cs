974944b4a3bd90003df43d808c2cd16a87f9f973
rgarcia <rgarcia@B11-R111E-12>
2017-01-24 13:32:49 -0800

    Add project files.

---
+++ HelloWorldMVC/Controllers/WelcomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Home Page";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Page";
+            return View();
+        }
+    }
+}
